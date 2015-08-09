a93a2c6302635ec490622c30d6cbc2a51e6b8b91
luodichen <me@luodichen.com>
2015-08-09 17:48:11 +0800

    interface for set dnspod account

--- wsgi/myproject/api/views.py
+++ wsgi/myproject/api/views.py
@@ -1,20 +1,18 @@
 import json
 import time
-from lib.response import JsonResponse
+from models import DNSPodAccount
 from models import User
+from lib.response import JsonResponse
 
 def active(request):
     response = {'code': 0, 'text': 'ok'}
-    token = None
-    ip_addr = None
     
-    try:
-        quest_data = json.loads(request.REQUEST.get('q'))
-        token = quest_data['token']
-        ip_addr = request.META['REMOTE_ADDR']
-    except:
+    token = request.REQUEST.get('token')
+    if token is None:
         response['code'], response['text'] = -1, 'bad request'
         return JsonResponse(response)
+    
+    ip_addr = request.META['REMOTE_ADDR']
     
     try:
         user = User.objects.get(token=token)
@@ -25,4 +23,29 @@ def active(request):
         response['code'], response['text'] = 1, 'token not exists'
     
     return JsonResponse(response)
+
+def dnspod_setup(request):
+    ret = {'code': 0, 'text': 'ok'}
+    
+    username = request.REQUEST.get('username')
+    password = request.REQUEST.get('password')
+    
+    if username is None or password is None:
+        ret['code'] = -1
+        ret['text'] = 'bad request'
+        return JsonResponse(ret)
+    
+    account = None
+    
+    if DNSPodAccount.objects.count() == 0:
+        account = DNSPodAccount()
+    else:
+        account = DNSPodAccount.objects.get()
+    
+    account.username = username
+    account.password = password
+    account.save()
+        
+    return JsonResponse(ret)
+
 
